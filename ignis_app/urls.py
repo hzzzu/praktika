2d0e81baa82e3261ae0b7b0fb2783053a34d07f5
Pavel <elkst0618@gmail.com>
2024-06-24 02:05:24 +0300

    add html carts

--- ignis_app/urls.py
+++ ignis_app/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('main.urls', namespace = 'main')),
     path('catalog/', include('goods.urls', namespace = 'catalog')),
     path('user/', include('users.urls', namespace = 'user')),
+    #path('cart/', include('carts.urls', namespace='cart')),
 ]
